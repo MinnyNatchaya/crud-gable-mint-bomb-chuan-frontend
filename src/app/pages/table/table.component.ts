5efbc20fabcc53e848e1f157550fe6f47af8e44b
Natchaya <007347@g-able.com>
2022-01-21 09:17:41 +0700

    connect database getListData

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -7,104 +7,104 @@ import { DataService } from 'src/app/services/data.service';
 
 const ELE_DATA: Data[] = [
   {
-    row_ID: '1',
-    par_ROW_ID: '2',
-    lov_TYPE: '1',
-    lov_NAME: 'LOV1',
-    display_VAL: 'display_VAL1',
-    lov_VAL1: '100.00',
-    lov_VAL2: '300.00',
-    lov_VAL3: '400.00',
-    lov_VAL4: '200.00',
-    lov_VAL5: '200.00',
-    lov_VAL6: '200.00',
-    lov_VAL7: '200.00',
-    lov_VAL8: '200.00',
-    lov_VAL9: '200.00',
-    lov_VAL10: '200.00',
-    active_FLG: '200.00',
-    text_DESC: 'Desc1',
-    order_BY: BigInt(100000),
-    modification_NUM: BigInt(100000),
-    created: new Date(Date.now() - 24 * 60 * 60 * 5000),
-    created_BY: 'Bomb',
-    last_UPD: new Date(Date.now() - 24 * 60 * 60 * 1000),
-    last_UPD_BY: 'mint',
-    group_TYPE: 'Group1',
+    rowId: '1',
+    parRowId: '2',
+    lovType: '1',
+    lovName: 'LOV1',
+    displayVal: 'display_VAL1',
+    lovVal1: '100.00',
+    lovVal2: '300.00',
+    lovVal3: '400.00',
+    lovVal4: '200.00',
+    lovVal5: '200.00',
+    lovVal6: '200.00',
+    lovVal7: '200.00',
+    lovVal8: '200.00',
+    lovVal9: '200.00',
+    lovVal10: '200.00',
+    activeFlg: '200.00',
+    textDesc: 'Desc1',
+    orderBy: BigInt(100000),
+    modificationNum: BigInt(100000),
+    create: new Date(Date.now() - 24 * 60 * 60 * 5000),
+    createBy: 'Bomb',
+    lastUpd: new Date(Date.now() - 24 * 60 * 60 * 1000),
+    lastUpdBy: 'mint',
+    groupType: 'Group1',
   },
   {
-    row_ID: '2',
-    par_ROW_ID: '2',
-    lov_TYPE: '2',
-    lov_NAME: 'LOV2',
-    display_VAL: 'display_VAL2',
-    lov_VAL1: '100.00',
-    lov_VAL2: '300.00',
-    lov_VAL3: '400.00',
-    lov_VAL4: '200.00',
-    lov_VAL5: '200.00',
-    lov_VAL6: '200.00',
-    lov_VAL7: '200.00',
-    lov_VAL8: '200.00',
-    lov_VAL9: '200.00',
-    lov_VAL10: '200.00',
-    active_FLG: '200.00',
-    text_DESC: 'Desc2',
-    order_BY: BigInt(200000),
-    modification_NUM: BigInt(200000),
-    created: new Date(Date.now() - 24 * 60 * 60 * 5000),
-    created_BY: 'Bomb',
-    last_UPD: new Date(Date.now() - 24 * 60 * 60 * 2000),
-    last_UPD_BY: 'mint',
-    group_TYPE: 'Group2',
+    rowId: '2',
+    parRowId: '2',
+    lovType: '2',
+    lovName: 'LOV2',
+    displayVal: 'display_VAL2',
+    lovVal1: '100.00',
+    lovVal2: '300.00',
+    lovVal3: '400.00',
+    lovVal4: '200.00',
+    lovVal5: '200.00',
+    lovVal6: '200.00',
+    lovVal7: '200.00',
+    lovVal8: '200.00',
+    lovVal9: '200.00',
+    lovVal10: '200.00',
+    activeFlg: '200.00',
+    textDesc: 'Desc2',
+    orderBy: BigInt(100000),
+    modificationNum: BigInt(100000),
+    create: new Date(Date.now() - 24 * 60 * 60 * 5000),
+    createBy: 'Bomb',
+    lastUpd: new Date(Date.now() - 24 * 60 * 60 * 2000),
+    lastUpdBy: 'mint',
+    groupType: 'Group2',
   },
   {
-    row_ID: '3',
-    par_ROW_ID: '2',
-    lov_TYPE: '3',
-    lov_NAME: 'LOV3',
-    display_VAL: 'display_VAL3',
-    lov_VAL1: '100.00',
-    lov_VAL2: '300.00',
-    lov_VAL3: '400.00',
-    lov_VAL4: '200.00',
-    lov_VAL5: '200.00',
-    lov_VAL6: '200.00',
-    lov_VAL7: '200.00',
-    lov_VAL8: '200.00',
-    lov_VAL9: '200.00',
-    lov_VAL10: '200.00',
-    active_FLG: '200.00',
-    text_DESC: 'Desc3',
-    order_BY: BigInt(30000000),
-    modification_NUM: BigInt(30000000),
-    created: new Date(Date.now() - 24 * 60 * 60 * 5000),
-    created_BY: 'Bomb',
-    last_UPD: new Date(Date.now() - 24 * 60 * 60 * 3000),
-    last_UPD_BY: 'mint',
-    group_TYPE: 'Group3',
+    rowId: '3',
+    parRowId: '3',
+    lovType: '3',
+    lovName: 'LOV3',
+    displayVal: 'display_VAL3',
+    lovVal1: '100.00',
+    lovVal2: '300.00',
+    lovVal3: '400.00',
+    lovVal4: '200.00',
+    lovVal5: '200.00',
+    lovVal6: '200.00',
+    lovVal7: '200.00',
+    lovVal8: '200.00',
+    lovVal9: '200.00',
+    lovVal10: '200.00',
+    activeFlg: '200.00',
+    textDesc: 'Desc3',
+    orderBy: BigInt(100000),
+    modificationNum: BigInt(100000),
+    create: new Date(Date.now() - 24 * 60 * 60 * 5000),
+    createBy: 'Bomb',
+    lastUpd: new Date(Date.now() - 24 * 60 * 60 * 3000),
+    lastUpdBy: 'mint',
+    groupType: 'Group3',
   },
 ];
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+// ];
 
 /**
  * @title Table with sorting
@@ -115,28 +115,28 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
-export class TableComponent implements AfterViewInit, OnInit {
+export class TableComponent implements OnInit {
   // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   // dataSource = new MatTableDataSource(ELEMENT_DATA);
   displayedColumns: string[] = [
-    'row_ID',
-    'par_ROW_ID',
-    'lov_TYPE',
-    'lov_NAME',
-    'display_VAL',
-    'active_FLG',
-    'text_DESC',
-    'order_BY',
-    'modification_NUM',
-    'created',
-    'created_BY',
-    'last_UPD',
-    'last_UPD_BY',
-    'group_TYPE',
+    'rowId',
+    'parRowId',
+    'lovType',
+    'lovName',
+    'displayVal',
+    'activeFlg',
+    'textDesc',
+    'orderBy',
+    'modificationNum',
+    'create',
+    'createBy',
+    'lastUpd',
+    'lastUpdBy',
+    'groupType',
     'actions',
   ];
-  dataSource = new MatTableDataSource(ELE_DATA);
   data: Data[] = [];
+  dataSource = new MatTableDataSource(this.data);
 
   constructor(
     private _liveAnnouncer: LiveAnnouncer,
@@ -145,14 +145,17 @@ export class TableComponent implements AfterViewInit, OnInit {
 
   @ViewChild(MatSort) sort: MatSort = new MatSort();
 
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-  }
+  // ngAfterViewInit() {
+  //   console.log(this.data);
+  //   this.dataSource.sort = this.sort;
+  // }
 
   ngOnInit(): void {
     this.dataService.getAll().subscribe(
       (res: any) => {
         this.data = res;
+        this.dataSource.sort = this.sort;
+        this.dataSource = new MatTableDataSource(this.data);
       },
       (err: any) => {
         console.dir(err);
